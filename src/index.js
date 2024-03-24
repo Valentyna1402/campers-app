7bc30b23c8ce9d22000be2aefa221768688242c7
Valentyna1402 <vriabchun@gmail.com>
2024-03-25 00:00:08 +0200

    run deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from './components/App';
 import './index.css';
 import { GlobalStyle } from './components/GlobalStyle';
-import { Provider } from 'react-redux';
 import { persistor, store } from '../src/redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
